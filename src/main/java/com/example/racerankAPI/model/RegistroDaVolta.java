4a6998187149e4133eaf9713c762b742396b7f34
irislobato <irisblobato@gmail.com>
2026-07-16 12:10:20 -0300

    atualizando package model
    esqueci de colocar o nome das colunas e o @Data na classe RegistroDaVolta

--- src/main/java/com/example/racerankAPI/model/RegistroDaVolta.java
+++ src/main/java/com/example/racerankAPI/model/RegistroDaVolta.java
@@ -1,20 +1,30 @@
 package com.example.racerankAPI.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class RegistroDaVolta {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToMany
+    @ManyToOne
+    @JoinColumn(name = "piloto_id", nullable = false)
     private Piloto piloto;
 
-    @OneToMany
+    @ManyToOne
+    @JoinColumn(name = "pista_id", nullable = false)
     private Pista pista;
+
+    @JoinColumn(name = "tempo_milissegundos")
     private Long tempo_milissegundos;
+
+    @JoinColumn(name = "data_volta")
     private LocalDateTime data_volta;
 }
