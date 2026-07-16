a125ad4ea1a46073df470fcb687f5a11d685c0f9
irislobato <irisblobato@gmail.com>
2026-07-16 17:04:04 -0300

    Criação do RegistroDaVoltaRepository

---
+++ src/main/java/com/example/racerankAPI/repository/RegistroDaVoltaRepository.java
@@ -0,0 +1,7 @@
+package com.example.racerankAPI.repository;
+
+import com.example.racerankAPI.model.RegistroDaVolta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegistroDaVoltaRepository extends JpaRepository<RegistroDaVolta, Long> {
+}
