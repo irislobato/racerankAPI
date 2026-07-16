336e3c4cb401e4d1c34d23dc7c3a640ca6262f4a
irislobato <irisblobato@gmail.com>
2026-07-16 17:21:56 -0300

    Uptade de metodos do registroDaVoltaRepository

--- src/main/java/com/example/racerankAPI/repository/RegistroDaVoltaRepository.java
+++ src/main/java/com/example/racerankAPI/repository/RegistroDaVoltaRepository.java
@@ -3,5 +3,14 @@ package com.example.racerankAPI.repository;
 import com.example.racerankAPI.model.RegistroDaVolta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+//Todo o CRUD é criado por padrão
 public interface RegistroDaVoltaRepository extends JpaRepository<RegistroDaVolta, Long> {
+
+    //Buscar todas as voltas de um piloto em específico
+    List<RegistroDaVolta> findByPilotoId(Long pilotoId);
+
+    //Busca a lista dos tempos das corridas em determinada pista da menor para a maior
+    List<RegistroDaVolta> findByPistaIdOrderByTempoMilissegundosAsc(Long pistaId);
 }
