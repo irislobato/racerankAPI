4bbcb46f3f3b4e6ffa063892606941e60c134286
irislobato <irisblobato@gmail.com>
2026-07-17 09:34:27 -0300

    Uptade PilotoService

--- src/main/java/com/example/racerankAPI/service/PilotoService.java
+++ src/main/java/com/example/racerankAPI/service/PilotoService.java
@@ -1,16 +1,27 @@
 package com.example.racerankAPI.service;
-
+import com.example.racerankAPI.dto.PilotoDto;
 import com.example.racerankAPI.model.Piloto;
+import com.example.racerankAPI.repository.PilotoRepository;
+import com.example.racerankAPI.repository.RegistroDaVoltaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PilotoService {
-    private final PilotoService pilotoRepository;
+    private final PilotoRepository pilotoRepository;
+    private final RegistroDaVoltaRepository registroDaVoltaRepository;
 
-    public PilotoService(com.example.racerankAPI.service.PilotoService pilotoRepository) {
+
+    public PilotoService(PilotoService pilotoRepository, RegistroDaVoltaRepository registroDaVoltaRepository) {
         this.pilotoRepository = pilotoRepository;
-    }
-    // ======================= CREATE =========================
-    public Piloto adicionarPiloto(){
+        this.registroDaVoltaRepository = registroDaVoltaRepository;
     }
 }
+// ======================= CREATE =========================
+public Piloto adicionarPiloto(PilotoDto dto){
+    if(dto.getNome() == null || dto.getNome().isEmpty())
+        throw new IllegalArgumentException("O campo do nome do piloto deve ser preenchido.");
+    if(dto.getNome() == null || dto.getNome().isEmpty())
+        throw new IllegalArgumentException("O campo do nome do piloto deve ser preenchido.");
+}
+}
+
