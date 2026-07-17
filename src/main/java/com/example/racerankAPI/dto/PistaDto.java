cb1bba5d4cb9d6b150ae36d1049e136ec6b63b36
irislobato <irisblobato@gmail.com>
2026-07-17 09:07:16 -0300

    Criação da camada de DTOs

---
+++ src/main/java/com/example/racerankAPI/dto/PistaDto.java
@@ -0,0 +1,11 @@
+package com.example.racerankAPI.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PistaDto {
+    private String nome;
+    private String localizacao;
+}
---
+++ src/main/java/com/example/racerankAPI/dto/RegistroDaVoltaDto.java
@@ -0,0 +1,4 @@
+package com.example.racerankAPI.dto;
+
+public class RegistroDaVoltaDto {
+}
