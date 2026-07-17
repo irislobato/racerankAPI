44ea26e4e3ce294d2e7b5d3258b9d6790f023a8d
irislobato <irisblobato@gmail.com>
2026-07-17 12:46:05 -0300

    registroDto

--- src/main/java/com/example/racerankAPI/dto/RegistroDaVoltaDto.java
+++ src/main/java/com/example/racerankAPI/dto/RegistroDaVoltaDto.java
@@ -1,4 +1,18 @@
 package com.example.racerankAPI.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class RegistroDaVoltaDto {
+
+    @JsonProperty("piloto_id")
+    private Long pilotoId;
+
+    @JsonProperty("pista_id")
+    private Long pistaId;
+
+    private Long tempoMilissegundos;
 }
--- src/main/java/com/example/racerankAPI/service/RegistroDaVoltaService.java
+++ src/main/java/com/example/racerankAPI/service/RegistroDaVoltaService.java
@@ -14,27 +14,39 @@ public class RegistroDaVoltaService {
     @Autowired
     private RegistroDaVoltaRepository registroDaVoltaRepository;
 
+    public RegistroDaVoltaService(RegistroDaVoltaRepository registroDaVoltaRepository) {
+        this.registroDaVoltaRepository = registroDaVoltaRepository;
+    }
+
+
+    //Create
     public RegistroDaVolta salvar(RegistroDaVolta registroDaVolta) {
+
         return registroDaVoltaRepository.save(registroDaVolta);
     }
 
+    //Read All
     public List<RegistroDaVolta> listarTodos() {
         return registroDaVoltaRepository.findAll();
     }
 
+    //Read By ID
     public Optional<RegistroDaVolta> buscarPorId(Long id) {
         return registroDaVoltaRepository.findById(id);
     }
 
+    //Read by PilotoId
     public List<RegistroDaVolta> buscarPorPiloto(Long pilotoId) {
         return registroDaVoltaRepository.findByPilotoId(pilotoId);
     }
 
+    //Read BY PistaId
     public List<RegistroDaVolta> rankingDaPista(Long pistaId) {
         return registroDaVoltaRepository.findByPistaIdOrderByTempoMilissegundosAsc(pistaId);
     }
 
-    public void excluir(Long id) {
+    //Delete
+    public void delete(Long id) {
         registroDaVoltaRepository.deleteById(id);
     }
 }
